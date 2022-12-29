c69d88ad48109d972f771413034333779e8b60f1
Zaenal Arif <zaenalarif05091999@gmail.com>
2022-12-29 12:39:26 +0700

    added a model. 1. Article 2. News Response 3. Source

---
+++ app/src/main/java/com/schoters/newsapp/model/Article.kt
@@ -0,0 +1,31 @@
+package com.schoters.newsapp.model
+
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+
+    @SerializedName("author")
+    var author: String?,
+
+    @SerializedName("title")
+    var title: String?,
+
+    @SerializedName("description")
+    var description: String?,
+
+    @SerializedName("url")
+    var url: String?,
+
+    @SerializedName("urlToImage")
+    var urlToImage: String?,
+
+    @SerializedName("publishedAt")
+    var publishedAt: String?,
+
+    @SerializedName("source")
+    var source: Source?
+) : Serializable
---
+++ app/src/main/java/com/schoters/newsapp/model/Source.kt
@@ -0,0 +1,11 @@
+package com.schoters.newsapp.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Source(
+    @SerializedName("id")
+    var id: String? = null,
+
+    @SerializedName("name")
+    var name: String
+)
