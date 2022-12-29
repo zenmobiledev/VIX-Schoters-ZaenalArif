95b6df570e65d36b6be569d5f628ecf6d68b4835
Zaenal Arif <zaenalarif05091999@gmail.com>
2022-12-30 05:58:49 +0700

    Add the table as entity.

--- app/src/main/java/com/schoters/newsapp/model/Article.kt
+++ app/src/main/java/com/schoters/newsapp/model/Article.kt
@@ -1,8 +1,11 @@
 package com.schoters.newsapp.model
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
+
+@Entity(tableName = "articles")
 
 data class Article(
     @PrimaryKey(autoGenerate = true)
