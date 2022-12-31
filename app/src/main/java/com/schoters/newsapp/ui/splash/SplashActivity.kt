41a9b7baed717a82db3fc0cffeae8148ea9f0741
Zaenal Arif <zaenalarif05091999@gmail.com>
2022-12-31 15:02:50 +0700

    Add an activity_splash.xml and SplashActivity.kt

---
+++ app/src/main/java/com/schoters/newsapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.schoters.newsapp.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.schoters.newsapp.MainActivity
+import com.schoters.newsapp.R
+
+class SplashActivity : AppCompatActivity() {
+    private val time: Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, time)
+    }
+}
